bdc52f4d2df4e0fe0442cf8e4b55deaaf89617d0
TedEngineer <tedvu184@gmail.com>
2024-01-31 13:39:11 +1100

    explore events

--- src/extension.ts
+++ src/extension.ts
@@ -5,21 +5,26 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "helloworld" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld" is now active!');
+  const didChange = vscode.workspace.onDidChangeTextDocument((e) => {
+    console.log(`DidChangeTextDocument`);
+  });
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    'helloworld.helloWorld',
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage('Hello World from Ted Vu');
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('helloworld.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from HelloWorld!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable, didChange);
 }
 
 // This method is called when your extension is deactivated
